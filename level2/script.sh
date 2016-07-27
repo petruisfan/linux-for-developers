dee932b79babc1b5a78a0a2034aff7d3860f5240
Petru Isfan <petru.isfan@gmail.com>
2016-07-27 23:56:38 +0300

    Added more samples

--- level2/script.sh
+++ level2/script.sh
@@ -4,45 +4,47 @@ AUX="123"
 ARG=${1:-${AUX:-"default value"}}
 #echo ${ARG}
 
-LINE="some long lien of text"
-#echo ${LINE:5:4}
+LINE="some long line of text"
+#echo "${LINE:5:4}"
 
 function print_ip() {
-    IP=$(
-        ifconfig ${1} | \
-            grep "inet addr:" |\
-            awk '{print $2}' |\
-            cut -f 2 -d ":"
-        )
-    echo ${IP}
+  IP=$(ifconfig ${1} |\
+    grep 'inet addr:' |\
+    awk '{print $2}' | \
+    cut -f 2 -d ":")
+  echo ${IP}
 }
 
 function long_running_task_1() {
-    sleep 1
+  sleep 1
 }
+
 function long_running_task_2() {
-    sleep 2
+  sleep 2
 }
 
 while [[ ! -z ${1} ]]; do
-    case "${1}" in
-        getip)
-            shift 
-            ARG=${1}
-            print_ip ${ARG}
-            shift
-            ;;
-        parallel)
-            long_running_task_1 &
-            long_running_task_2 &
-            PID=$!
-            wait ${PID}
-            shift
-            ;;
-        *)
-            echo "Invalid argument"
-            exit 1
-    esac
-    
-done    
-
+  case "$1" in
+    --ip|-i)
+      print_ip ${2}
+      shift
+      shift
+      ;;
+    --parallel|-p)
+      long_running_task_1 &
+      long_running_task_2 &
+      PID=$!
+      wait ${PID}
+      notify-send script.sh "execution finished"
+      shift
+      ;;
+    --help|-h)
+      echo "This is a help message"
+      shift
+      ;;
+    *)
+      echo "Invalid option"
+      exit 1
+      ;;
+  esac
+done
