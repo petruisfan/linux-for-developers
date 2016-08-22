40e80ea80708a4b9a07e6381cbb0616fa3b7db14
Petru Isfan <petru.isfan@gmail.com>
2016-08-22 08:49:18 +0300

    Updated vim

--- level3/configure_vim.sh
+++ level3/configure_vim.sh
@@ -15,3 +15,51 @@ git clone https://github.com/honza/vim-snippets.git
 
 # xmllint
 sudo apt install libxml2-utils
+
+cat > ~/.vimrc << EOF
+set nocompatible
+filetype off
+
+" Settings to replace tab. Use :retab for replacing tab in existing files.
+set tabstop=4
+set shiftwidth=4
+set expandtab
+
+" Have Vim jump to the last position when reopening a file
+if has("autocmd")
+   au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
+endif
+
+" Other general vim options:
+syntax on
+set showmatch      " Show matching brackets.
+set ignorecase     " Do case insensitive matching
+set incsearch      " show partial matches for a search phrase
+set nopaste
+set number         " show line number
+set undolevels=1000
+
+" Always show the status line
+set laststatus=2
+
+" Format the status line
+set statusline=\ %{HasPaste()}%F%m%r%h\ %w\ \ CWD:\ %r%{getcwd()}%h\ \ \ Line:\ %l\ \ Column:\ %c
+
+" Returns true if paste mode is enabled
+function! HasPaste()
+    if &paste
+        return 'PASTE MODE  '
+    en
+    return ''
+endfunction
+
+colorscheme desert
+
+map j !python -m json.tool<CR>              " format JSON
+map l !xmllint --format --recover -<CR>     " format XML
+
+execute pathogen#infect()
+
+set cryptmethod=blowfish2
+EOF
+
